b2efc0b3bc9bde5ecab23352d7242e1ee6aa974e
abdullateef <abdullateefopeyemi06@gmail.com>
2025-12-22 22:53:48 +0100

    removing fatal

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -89,17 +89,3 @@ export default function Checkout() {
     </div>
   );
 }
-
-
-fatal: No configured push destination.
-Either specify the URL from the command-line or configure a remote repository using
-
-    git remote add <name> <url>
-
-and then push using the remote name
-
-    git push <name>
-
-PS C:\Users\PC\Desktop\dcan-project> ^C             
-PS C:\Users\PC\Desktop\dcan-project> ^C
-PS C:\Users\PC\Desktop\dcan-project> ^C
